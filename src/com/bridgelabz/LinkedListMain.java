2f94cbd760d4fbc51364f5029d1c0347424ec99d
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-10 12:21:29 +0530

    Added :Created linkedlist

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
 
-public class LinkedListMain {
+import java.util.LinkedList;
+
+public class LinkedListMain{
     public static void main(String[] args) {
-        System.out.println("Welcome to LinkedList Data Structure");
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        System.out.println("Elements in the list are :" + list);
     }
 }
