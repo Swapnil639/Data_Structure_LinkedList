930caedeb42ab545dc1e99caaa6efaa909ef4b12
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-10 12:36:58 +0530

    Added :Appended elements to the list

--- src/com/bridgelabz/LinkedListFunctions.java
+++ src/com/bridgelabz/LinkedListFunctions.java
@@ -2,12 +2,25 @@ package com.bridgelabz;
 
 public class LinkedListFunctions {
     Node head;
-
     public void addFirst(int data) {
         Node newNode = new Node(data);
         newNode.next = head;
         head = newNode;
     }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node lastNode = head;
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+        }
+        lastNode.next = newNode;
+    }
+
 
     public void printList() {
         Node currNode = head;
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -5,8 +5,8 @@ public class LinkedListMain{
     public static void main(String[] args) {
        LinkedListFunctions list=new LinkedListFunctions();
         list.addFirst(56);
-        list.addFirst(30);
-        list.addFirst(70);
+        list.addLast(30);
+        list.addLast(70);
         System.out.println("Elements in the list are :");
         list.printList();
     }
